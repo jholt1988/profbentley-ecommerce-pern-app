31f4e16823881e44073b08d5096a1eaeff03ecd3
Jordan A Holt <jholt316@gmail.com>
2022-07-25 06:44:04 -0500

    Create folders for Routes, Components, and Store.  Created React Router. Next work on store

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Switch,
+    Route
+} from "react-router-dom";
+
+
+function App() {
+    return (
+        <div>
+            <Router>
+                
+            </Router>
+        </div>
+    )
+}
+
+export default App
